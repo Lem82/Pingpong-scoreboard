ca801f2677e6c84cd60584e62bd0f74bb9bb2a85
Tommi Kaura-Aho <tommi.kaura-aho@pinja.com>
2022-04-08 10:59:16 +0300

    Display player names

--- src/modules/score/ScoreContainer.tsx
+++ src/modules/score/ScoreContainer.tsx
@@ -1,9 +1,17 @@
+import { useAppSelector } from '../../core/hooks';
 import './Score.scss';
 
 function ScoreContainer(): JSX.Element {
+    const score = useAppSelector(state => state.score);
+
     return (
         <div id="score-container">
-            score goe shere
+            <div className="left-player player">
+                {score.leftPlayer.name}
+            </div>
+            <div className="right-player player">
+                {score.rightPlayer.name}
+            </div>
         </div>
     );
 }
